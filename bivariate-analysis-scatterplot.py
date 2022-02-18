825bb1366eb857db363e6879916180a7b21644a9
cbarna22 <99765996+cbarna22@users.noreply.github.com>
2022-02-18 17:29:25 -0500

    Add files via upload

---
+++ bivariate-analysis-scatterplot.py
@@ -0,0 +1,27 @@
+#Python Exploratory Data Analysis Project
+#Histograms
+#
+#This script displays histograms for all columns in the dataset
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
+
+
+df = pd.read_csv('/Users/Camille/Downloads/home_data.csv')          #reading in dataset to dataframe
+
+df['date'] = pd.to_datetime(df['date'])                             #changing date to datetime object
+
+pd.set_option("display.max_columns", 100)
+
+df.set_index('date', inplace=True)                                  #setting date as index
+
+
+sns.scatterplot(x=df['sqft_living'], y=df['price'])                 #creating and displaying scatterplot
+plt.show()
+
+
+
+
